b1ad19892e7eb0b0ac113de91fb6b20de35a1d12
祝轶 <zhuyi535@pingan.com.cn>
2019-02-27 20:36:08 +0800

    fix bugs

---
+++ Alpha/Utility/Categories/Foundation/NSBundle+Yzt.h
@@ -0,0 +1,17 @@
+//
+//  NSBundle+Yzt.h
+//  Alpha
+//
+//  Created by easy on 2019/2/27.
+//  Copyright © 2019 Unified Sense. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSBundle (Yzt)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Alpha/Utility/Categories/Foundation/NSBundle+Yzt.m
@@ -0,0 +1,13 @@
+//
+//  NSBundle+Yzt.m
+//  Alpha
+//
+//  Created by easy on 2019/2/27.
+//  Copyright © 2019 Unified Sense. All rights reserved.
+//
+
+#import "NSBundle+Yzt.h"
+
+@implementation NSBundle (Yzt)
+
+@end
